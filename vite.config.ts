b6f2dff534701f2687b91d3693d7db4a8afa98d3
Steven Bennett <steven@budgetbloom.com>
2022-03-05 09:17:15 -0800

    External

--- vite.config.ts
+++ vite.config.ts
@@ -25,6 +25,7 @@ export default defineConfig({
         'amazon-cognito-identity-js',
         '@srclaunch/web-application-state',
         '@srclaunch/exceptions',
+        '@srclaunch/icons',
         '@srclaunch/validation',
         '@srclaunch/logger',
         'i18n-iso-countries',
